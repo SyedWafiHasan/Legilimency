ebccd5cfc29d040637047a18fcd01b3013f0fc70
Syed Wafi Hasan <syedwafihasan@gmail.com>
2017-07-03 03:03:55 +0530

    Version 4.0

--- app/src/main/java/com/wafihasan/legilimency/MainActivity.java
+++ app/src/main/java/com/wafihasan/legilimency/MainActivity.java
@@ -1,17 +1,18 @@
 package com.wafihasan.legilimency;
 
-import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,15 +20,14 @@ public class MainActivity extends AppCompatActivity
 {
     String []a = {"Think of a number", "Double it", "Add","Divide it by 2", "Subtract the original number"};
     int x = a.length;
-    String t = "To it.";
+    String t = "To it";
     int c = 0;
     int j = 0;
 
     public int random()
     {
         Random r = new Random();
-        int n = r.nextInt(51)*2;
-        return n;
+        return r.nextInt(51)*2;
     }
 
     @Override
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity
         final TextView tv3 = (TextView)findViewById(R.id.tv3);
         final TextView tv4 = (TextView)findViewById(R.id.tv4);
 
-        tv1.setText("Click to start");
+        tv1.setText(R.string.start);
         tv3.setText("");
         tv4.setText("");
 
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity
         {
             public void onClick(View lin)
             {
-                tv2.setText("Click to continue");
+                tv2.setText(R.string.click_continue);
                 if(c == 2)
                 {
                     tv1.setText(a[c]);
@@ -73,9 +73,9 @@ public class MainActivity extends AppCompatActivity
                 else if (c == x)
                 {
                     c = 0;
-                    tv1.setText("The number in your mind is");
+                    tv1.setText(R.string.number_mind);
                     tv3.setText(Integer.toString((j/2)));
-                    tv2.setText("Click to try again");
+                    tv2.setText(R.string.try_again);
                 }
                 else
                 {
@@ -99,21 +99,53 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+
+        Typeface pacman = Typeface.createFromAsset(getAssets(),"fonts/pacman.ttf");
+        Typeface less = Typeface.createFromAsset(getAssets(),"fonts/less.ttf");
+
         if(item.getItemId() == R.id.about)
         {
-            ad.setIcon(R.mipmap.ic_launcher);
-            ad.setTitle("About The Developer");
-            ad.setMessage("Developed by Wafi Hasan.\nA student and tech enthusiast.");
-            AlertDialog ad1 = ad.create();
-            ad1.show();
+            AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+            View v = getLayoutInflater().inflate(R.layout.custom_about_dialog, null);
+
+            ImageView iv = (ImageView)v.findViewById(R.id.imageView2);
+            TextView tv5 = (TextView)v.findViewById(R.id.heading);
+            TextView tv6 = (TextView)v.findViewById(R.id.actual_text);
+
+            tv5.setTypeface(pacman);
+            tv6.setTypeface(less);
+
+            ad.setView(v);
+            ad.show();
         }
         else if(item.getItemId() == R.id.contact)
         {
-            ad.setIcon(R.mipmap.ic_launcher);
-            ad.setTitle("Contact the Developer");
-            ad.setMessage("You can reach the developer at :\n\nfacebook.com/syedwafihasan\n\ninstagram.com/syedwafihasan\n\nsyedwafihasan@gmail.com\n\n+918299841298\n\n+917071769357");
-            AlertDialog ad1 = ad.create();
+            AlertDialog.Builder ad1 = new AlertDialog.Builder(MainActivity.this);
+            View v1 = getLayoutInflater().inflate(R.layout.custom_contact_dialog, null);
+
+            ImageView iv2 = (ImageView)v1.findViewById(R.id.imageView2);
+            TextView tv7 = (TextView)v1.findViewById(R.id.contact_heading);
+            TextView tv8 = (TextView)v1.findViewById(R.id.actual_text_contact);
+            TextView tv9 = (TextView)v1.findViewById(R.id.fb);
+            TextView tv10 = (TextView)v1.findViewById(R.id.insta);
+            TextView tv11 = (TextView)v1.findViewById(R.id.mail);
+
+            tv7.setTypeface(pacman);
+            tv8.setTypeface(less);
+            tv9.setTypeface(less);
+            tv10.setTypeface(less);
+            tv11.setTypeface(less);
+
+            tv10.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(MainActivity.this, "Instagram doesn't support opening profiles directly. Only posts. You can click my name on the top to contact me.", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            ad1.setView(v1);
             ad1.show();
         }
         return super.onOptionsItemSelected(item);
